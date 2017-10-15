a851cf02090a0e1120322d11e07790e181406e32
Louis-7 <louisgh.cn@gmail.com>
2017-10-15 12:06:35 +0800

    [feat] Add search file content feature.

--- src/business/utils/utils.js
+++ src/business/utils/utils.js
@@ -41,56 +41,70 @@ let filterFileByOptions = (fileList, xFilterOptions) => {
     options = fileFilterOptionsOptimize(options);
 
     for (let file of fileList) {
-        promiseList.push(statp(file).then((stat) => {
-            let pass = true;
+        promiseList.push(statp(file)
+            .then((stat) => {
+                let pass = true;
 
-            //check mtime
-            if (options.mtime) {
-                let mtimeOperator = ['+', '-'].indexOf(options.mtime.substr(0, 1)) > -1 ? options.mtime.substr(0, 1) : '=',
-                    pastDays = parseInt(['+', '-'].indexOf(options.mtime.substr(0, 1)) > -1 ? options.mtime.substr(1) : options.mtime),
-                    targetTime = moment(`${moment().year()}-${moment().month()}-${moment().date()}`,'YYYY-M-D').subtract(pastDays, 'days'),
-                    momentMTime = `${stat.mtime.getFullYear()}-${stat.mtime.getMonth()}-${stat.mtime.getDate()}`;
+                // check if it's a file
+                if (stat.isDirectory()) pass = false;
 
-                switch (mtimeOperator) {
-                    case '+':
-                        if (!moment(momentMTime,'YYYY-M-D').isBefore(targetTime)) pass = false;
-                        break;
-                    case '-':
-                        if (!moment(momentMTime,'YYYY-M-D').isAfter(targetTime)) pass = false;
-                        break;
-                    case '=':
-                        if (!moment(momentMTime,'YYYY-M-D').isSame(targetTime)) pass = false;
-                        break;
-                    default:
-                        throw 'Operator for mtime is not right'
+                //check mtime
+                if (options.mtime && pass) {
+                    let mtimeOperator = ['+', '-'].indexOf(options.mtime.substr(0, 1)) > -1 ? options.mtime.substr(0, 1) : '=',
+                        pastDays = parseInt(['+', '-'].indexOf(options.mtime.substr(0, 1)) > -1 ? options.mtime.substr(1) : options.mtime),
+                        targetTime = moment(`${moment().year()}-${moment().month()}-${moment().date()}`, 'YYYY-M-D').subtract(pastDays, 'days'),
+                        momentMTime = `${stat.mtime.getFullYear()}-${stat.mtime.getMonth()}-${stat.mtime.getDate()}`;
+
+                    switch (mtimeOperator) {
+                        case '+':
+                            if (!moment(momentMTime, 'YYYY-M-D').isBefore(targetTime)) pass = false;
+                            break;
+                        case '-':
+                            if (!moment(momentMTime, 'YYYY-M-D').isAfter(targetTime)) pass = false;
+                            break;
+                        case '=':
+                            if (!moment(momentMTime, 'YYYY-M-D').isSame(targetTime)) pass = false;
+                            break;
+                        default:
+                            throw 'Operator for mtime is not right'
+                    }
                 }
-            }
 
-            // check size
-            if (options.size && pass) {
-                let sizeOperator = ['+', '-'].indexOf(options.size.substr(0, 1)) > -1 ? options.size.substr(0, 1) : '=',
-                    sizeUnit = ['k', 'm'].indexOf(options.size.substr(-1, 1)) > -1 ? options.size.substr(-1, 1) : 'k',
-                    fileSize = formatSize(options.size, sizeUnit);
+                // check size
+                if (options.size && pass) {
+                    let sizeOperator = ['+', '-'].indexOf(options.size.substr(0, 1)) > -1 ? options.size.substr(0, 1) : '=',
+                        sizeUnit = ['k', 'm'].indexOf(options.size.substr(-1, 1)) > -1 ? options.size.substr(-1, 1) : 'k',
+                        fileSize = formatSize(options.size, sizeUnit);
 
-                switch (sizeOperator) {
-                    case '+':
-                        if (stat.size <= fileSize) pass = false;
-                        break;
-                    case '-':
-                        if (~stat.size >= fileSize) pass = false;
-                        break;
-                    case '=':
-                        if (stat.size !== fileSize) pass = false;
-                        break;
-                    default:
-                        throw 'Operator for file is not right'
+                    switch (sizeOperator) {
+                        case '+':
+                            if (stat.size <= fileSize) pass = false;
+                            break;
+                        case '-':
+                            if (~stat.size >= fileSize) pass = false;
+                            break;
+                        case '=':
+                            if (stat.size !== fileSize) pass = false;
+                            break;
+                        default:
+                            throw 'Operator for file is not right'
+                    }
                 }
-            }
 
-            if (pass) {
-                finalList.push(file);
-            }
-        }))
+                return pass;
+            })
+            .then((filePass) => {
+                //check file content
+                if (filePass && options.text) {
+                    return readFilep(file).then((content) => {
+                        if (content.indexOf(options.text) > -1) {
+                            finalList.push(file);
+                        }
+                    })
+                } else if (filePass) {
+                    finalList.push(file);
+                }
+            }))
     }
 
     return Promise.all(promiseList).then(() => {
@@ -110,7 +124,7 @@ let formatSize = (size, unit) => {
     // convert mb to kb
     if (unit === 'm') {
         size *= 1024 * 1024;
-    }else{
+    } else {
         size *= 1024;
     }
 
@@ -160,6 +174,15 @@ let readdirp = (directory, options) => {
         });
 
     });
+};
+
+let readFilep = (filePath, options) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, options, (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        })
+    })
 };
 
 let globp = (pattern, options) => {
